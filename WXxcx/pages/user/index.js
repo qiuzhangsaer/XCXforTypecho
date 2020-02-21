b76aa080c1be63418760758c7d233d13429db883
心灵导师安德烈 <qiuzhangsaer@qq.com>
2020-02-21 14:08:30 +0800

    更新微信小程序源码

---
+++ WXxcx/pages/user/index.js
@@ -0,0 +1,23 @@
+//获取应用实例
+const app = getApp()
+
+Page({
+  data: {
+    canIUse: wx.canIUse('button.open-type.getUserInfo'),
+  },
+  onLoad: function () {},
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  },
+  //赞赏支持
+  showSupport() {
+    wx.previewImage({
+      urls: ['https://wx.xvkes.cn/paycode.jpg'],
+      current: 'https://wx.xvkes.cn/paycode.jpg' // 当前显示图片的http链接      
+    })
+  },
+
+})
